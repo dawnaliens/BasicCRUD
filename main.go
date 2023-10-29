81daa1b742b5b94cbce0c0bd78cb27e3371bf07d
Richard Liu <richardliux23@gmail.com>
2023-10-29 23:25:27 +0800

    Push some codes

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func ExistInArray(target int, array []int) bool {
+	for i := 0; i < len(array); i++ {
+		if target == array[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func ExistInAnArray(target int, array []int) bool {
+	for _, v := range array {
+		if target == v {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	fmt.Println("Hello World")
-
+	
 }
